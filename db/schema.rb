2401305c0106f1e41e7c1eea3181110b13e47767
TahirAhmad <tahirahmad0234@gmail.com>
2024-05-04 20:57:48 +0600

    post table & model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_04_093755) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_04_141503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,20 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_04_093755) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_blacklisted_tokens_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "short_description"
+    t.string "body"
+    t.json "tags"
+    t.string "slug"
+    t.string "blog_status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "revoked_tokens", force: :cascade do |t|
@@ -48,5 +62,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_04_093755) do
   end
 
   add_foreign_key "blacklisted_tokens", "users"
+  add_foreign_key "posts", "users"
   add_foreign_key "revoked_tokens", "users"
 end
