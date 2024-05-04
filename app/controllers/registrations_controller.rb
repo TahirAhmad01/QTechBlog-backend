be5c17e69c309e0f07d65c76ff5699df631e6dde
TahirAhmad <tahirahmad0234@gmail.com>
2024-05-04 16:01:01 +0600

    fix registrations

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,20 +9,23 @@ class RegistrationsController < Devise::RegistrationsController
         status: 401
       }
     else
-      super do |user|
-        if user.persisted?
-          token = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil)
-          render json: { message: "User registration was successfully registered", user: user, access_token: token }, status: :ok
-          return
-        else
-          errors_array = resource.errors.messages.map do |attribute, messages|
-            { name: attribute, errors: messages }
-          end
-          render json: {
-            error: "User registration failed", error_messages: errors_array
-          }, status: :unprocessable_entity
-        end
+      super
+    end
+  end
+
+  private
+
+  def respond_with (resource, options = {})
+    if resource.persisted?
+      token = Warden::JWTAuth::UserEncoder.new.call(resource, :resource, nil)
+      render json: { message: "User registration was successfully registered", user: resource, access_token: token }, status: :ok
+    else
+      errors_array = resource.errors.messages.map do |attribute, messages|
+        { name: attribute, errors: messages }
       end
+      render json: {
+        error: "User registration failed", error_messages: errors_array
+      }, status: :unprocessable_entity
     end
   end
 end
