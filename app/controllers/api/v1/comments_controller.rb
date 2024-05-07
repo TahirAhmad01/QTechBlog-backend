47d2b0c97724a88ce8d39d73429595615e0bcab6
TahirAhmad <tahirahmad0234@gmail.com>
2024-05-07 23:24:45 +0600

    add comment pagination, detroy method & add validation commnents

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,42 +1,57 @@
 class Api::V1::CommentsController < ApiController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   before_action :set_blog
   before_action :set_comment, only: [:show, :update, :destroy]
+  before_action :set_page, only: [:index]
 
   def index
-    @comments = @blog.comments.includes(:replies)
-    render json: @comments.map { |comment| format_comment(comment, 3) }
+    @parent_comments = @blog.comments.where(parent_id: nil)
+                            .includes(replies: { replies: :replies })
+                            .order(id: :asc).paginate(page: params[:page], per_page: params[:per_page].to_i.positive? ? params[:per_page].to_i : 10)
+
+    sorted_comments = sort_comments(@parent_comments)
+    pagination_info = {
+      total_pages: @parent_comments.total_pages,
+      current_page: @parent_comments.current_page,
+      next_page: @parent_comments.next_page,
+      prev_page: @parent_comments.previous_page
+    }
+
+    render json: { status: "success", data: sorted_comments.map { |comment| format_comment_with_replies(comment) }, pagination: pagination_info }
   end
 
   def show
     if @comment
-      render json: format_comment(@comment, 3)
+      render json: { status: "success", data: format_comment_with_replies(@comment) }
     else
       render json: { error: 'Comment not found' }, status: :not_found
     end
   end
 
-
   def create
     @comment = @blog.comments.new(comment_params)
     @comment.user_id = current_user.id
     if @comment.save
-      render json: @comment, status: :created
+      render json: { status: "success", message: "Blog create successfully", data: @comment}, status: :created
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: { status: "Failed", message: "Blog create Failed", error: @comment.errors.full_messages}, status: :unprocessable_entity
     end
   end
 
   def update
-    if @comment.update(comment_params)
-      render json: @comment
+    if @comment.update(comment_update_params)
+      render json: { status: "success", message: "Comment update successfully", data: @comment }, status: :ok
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json: { status: "error", message: "Comment update failed", error: @comment.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @comment.destroy
-    head :no_content
+    if @comment.destroy
+      render json: { message: 'Comment deleted successfully', data: @comment }, status: :ok
+    else
+      render json: { status: "error", message: "Comment delete failed", error: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
@@ -53,24 +68,40 @@ class Api::V1::CommentsController < ApiController
     render json: { error: 'Comment not found' }, status: :not_found
   end
 
+  def set_page
+    @page = params[:page]&.to_i || 1
+  end
+
   def comment_params
     params.require(:comment).permit(:comment, :parent_id)
   end
 
-  def format_comment(comment, level)
-    return nil if level.zero?
+  def comment_update_params
+    params.require(:comment).permit(:comment)
+  end
 
-    parent_comment_id = comment.parent_id
-    parent_comment_id = nil if parent_comment_id.blank?
+  def sort_comments(comments)
+    sorted_comments = comments.to_a.sort_by(&:id)
+    sorted_comments.each do |comment|
+      comment.replies = comment.replies.sort_by(&:id)
+      comment.replies.each do |reply|
+        reply.replies = reply.replies.sort_by(&:id)
+      end
+    end
+    sorted_comments
+  end
+
+  def format_comment_with_replies(comment)
     {
       id: comment.id,
       comment: comment.comment,
-      parent_comment_id: parent_comment_id,
+      parent_comment_id: comment.parent_id,
       blog_id: comment.blog_id,
       user_id: comment.user_id,
       created_at: comment.created_at,
       updated_at: comment.updated_at,
-      replies: comment.replies.map { |reply| format_comment(reply, level - 1) }
+      replies: comment.replies.map { |reply| format_comment_with_replies(reply) }
     }
   end
+
 end
